c57aa9b143636a35ac62a74589d787d33ae2d180
maross3 <maross334@gmail.com>
2023-04-22 13:56:12 -0400

    radix_sort.rb: style refactor

--- Ruby/Sort/radix_sort.rb
+++ Ruby/Sort/radix_sort.rb
@@ -2,21 +2,21 @@
 
 module RadixSort
   def self.sort(array)
-    maxLen = GetMaxLength(array)
+    maxLen = get_max_length(array)
     (0..maxLen - 1).each do |i|
       buckets = Array.new(10) { Array.new }
-      array.each do |j| buckets[GetDigit(j, i)].push(j) end
-      array = Flatten(buckets)
+      array.each do |j| buckets[get_digit(j, i)].push(j) end
+      array = flatten(buckets)
     end
     return array
   end
 
-  def self.GetDigit(number, index)
+  def self.get_digit(number, index)
     divisor = 10 ** index
     return (number / divisor) % 10
   end
 
-  def self.Flatten(buckets)
+  def self.flatten(buckets)
     array = []
     buckets.each do |k|
       array.push(k)
@@ -25,7 +25,7 @@ module RadixSort
     return array
   end
 
-  def self.GetMaxLength(array)
+  def self.get_max_length(array)
     max = array.max
     return max.to_s.length
   end
